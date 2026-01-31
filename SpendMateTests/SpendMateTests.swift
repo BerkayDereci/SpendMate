c8ccf015e25fc7cacf0d2dec8569e98b51af7572
berkaydereci <BERKAY.DERECI@lcwaikiki.com>
2026-01-31 23:40:35 +0300

    Initial Commit

---
+++ SpendMateTests/SpendMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpendMateTests.swift
+//  SpendMateTests
+//
+//  Created by Berkay Dereci on 31.01.2026.
+//
+
+import Testing
+@testable import SpendMate
+
+struct SpendMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
